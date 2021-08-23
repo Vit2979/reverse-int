584b66b136bbb48ac3035d3c87131ecdb852f631
Vit2979 <86978620+Vit2979@users.noreply.github.com>
2021-08-24 00:38:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 
-
-module.exports = function reverse (n) 
-{
-    let result = n[n.length -1];
-    for(let i = n.length - 2; i >= 0; i=i-1)
-    result = result + n[i];
+module.exports = function reverse (n) {
+ n = String(n);
+ let a = n.split('').reverse().join('');
+ let result = Number.parseInt(a);
  return result;
 }
